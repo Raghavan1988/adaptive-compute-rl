9a447e51f5a696b88b6be8b1d7bdc7a2a3e8475a
Raghavan Muthuregunathan <raghavan.mit@gmail.com>
2026-08-31 22:27:27 -0700

    add __init__

--- src/when_to_think/utils/__init__.py
+++ src/when_to_think/utils/__init__.py
@@ -1,1 +1,24 @@
 """Seeding, run records, config, and shared helpers."""
+
+from when_to_think.utils.run_record import (
+    RunRecord,
+    create_run_record,
+    generate_run_id,
+    get_git_commit,
+    get_git_dirty,
+    run_dir_for,
+    write_run_record,
+)
+from when_to_think.utils.seeding import make_generator, seed_everything
+
+__all__ = [
+    "RunRecord",
+    "create_run_record",
+    "generate_run_id",
+    "get_git_commit",
+    "get_git_dirty",
+    "make_generator",
+    "run_dir_for",
+    "seed_everything",
+    "write_run_record",
+]
