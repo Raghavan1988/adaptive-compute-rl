b388c88964c69814c2c049b8e43d8e06b9229dea
Raghavan Muthuregunathan <raghavan.mit@gmail.com>
2026-09-02 22:47:59 -0700

    update __init__

--- src/when_to_think/representations/__init__.py
+++ src/when_to_think/representations/__init__.py
@@ -6,8 +6,11 @@ from when_to_think.representations.extraction import (
     extract_hidden_states,
     pool_hidden,
 )
+from when_to_think.representations.reader import HiddenStateKey, HiddenStateReader
 
 __all__ = [
+    "HiddenStateKey",
+    "HiddenStateReader",
     "RepresentationDescriptor",
     "ShardedRepresentationWriter",
     "extract_hidden_states",
